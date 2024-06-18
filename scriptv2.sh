b6c5085dd3fff794deb492c8af75a28a7ad2cb79
Abobora <lucasclucas7@gmail.com>
2024-06-18 09:34:04 -0300

    Update scriptv2.sh

--- scriptv2.sh
+++ scriptv2.sh
@@ -121,7 +121,7 @@ git commit -m "Initial commit"
 
 echo "Node.js project with Fastify, dotenv, EditorConfig, Prettier, ESLint, and TypeScript successfully configured!"
 
-alias fastify='sh -c "$(curl -fsSL https://raw.githubusercontent.com/Lucas-Winicius/create-fastify-project/main/scriptv2.sh)"'
+alias fastify='bash -c "$(curl -fsSL https://raw.githubusercontent.com/Lucas-Winicius/create-fastify-project/main/scriptv2.sh)"'
 
 echo "Now you can use: fastify <project-name>"
 
